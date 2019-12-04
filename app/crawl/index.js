56b2e3bf7a7a0b63deac0d79235ec2d05a9c4591
zhaojianbo <zhaojianbo@meipinmi.com.cn>
2019-12-04 17:48:48 +0800

    pachong

---
+++ app/crawl/index.js
@@ -0,0 +1,58 @@
+const cheerio = require('cheerio')
+const iconv   = require('iconv-lite')
+const https   = require('https')
+
+// const targetUrl = 'https://www.imooc.com/'
+const targetUrl = 'https://www.dytt8.net/index0.html'
+const target = ['中国机长', '杀手']
+
+const main = async () => {
+  https.get(targetUrl, (res) => {
+    let chunks = []
+
+    res.on('data', function(chunk) {
+      chunks.push(chunk)
+    })
+
+    res.on('end', function(res) {
+      let html = iconv.decode(Buffer.concat(chunks),'gb2312')
+      let $ = cheerio.load(html)
+      let result = []
+      $('.co_content2').find('a').each((index, item) => {
+        const text = $(item).text()
+        console.log(text)
+        target.forEach(t => {
+          if (text.indexOf(t) !== -1) {
+
+            // result.push($(item).attr('href'))
+            result.push(text)
+          }
+        })
+
+      })
+      console.log(result)
+    })
+
+  }).on('error', function(err) {
+    console.log(err)
+  })
+}
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 确定目标 要爬取哪个网站
+// 分析目标 url格式 数据格式 网页编码
+// 编写代码
+// 执行爬虫
